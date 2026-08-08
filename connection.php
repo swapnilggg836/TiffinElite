338272e91024aedf9b9fcb4af303234b7a36be2e
Swapnil Gaikwad <swapnilg836@gmail.com>
2026-08-08 15:55:06 +0530

    Add DB_PORT support in connection.php

--- connection.php
+++ connection.php
@@ -1,14 +1,15 @@
 <?php
 $host     = getenv('DB_HOST') ?: "localhost";
+$port     = getenv('DB_PORT') ?: 3306;
 $username = getenv('DB_USER') ?: "root";
 $password = getenv('DB_PASS') !== false ? getenv('DB_PASS') : "";
 $db       = getenv('DB_NAME') ?: "yum";
 
-$conn = new mysqli($host, $username, $password, $db);
+$conn = new mysqli($host, $username, $password, $db, (int)$port);
 
 try {
     // Create a PDO connection
-    $pdo = new PDO("mysql:host=$host;dbname=$db", $username, $password);
+    $pdo = new PDO("mysql:host=$host;port=$port;dbname=$db", $username, $password);
     // Set PDO error mode to exception
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
